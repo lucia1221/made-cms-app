fb1a1f067bb109848057fb8009371fd5b6c79da6
lucia1221 <58188855+lucia1221@users.noreply.github.com>
2022-01-11 15:20:59 +0100

    simple data loading (#2)
    Co-authored-by: Lucia Badinova <>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,23 @@
+import { Link, LoaderFunction, useLoaderData } from "remix";
+
+type Article = {
+  id: number;
+  title: string;
+};
+
+export const loader: LoaderFunction = (): Article[] => {
+  return [
+    { id: 1, title: "slovaci vyhrali" },
+    { id: 2, title: "slovaci prehrali" },
+  ];
+};
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+  const articles = useLoaderData<Article[]>();
+
+  return articles.map((a) => (
+    <div key={a.id}>
+      <Link to={`/article/${a.id}`}>{a.title}</Link>
     </div>
-  );
+  ));
 }
