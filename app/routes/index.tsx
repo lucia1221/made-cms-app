88f2d315019eae90bf84a33aa8a237f9751dae0e
Lucia Badinova <unknown>
2022-01-11 15:54:32 +0100

    Load articles from files

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,21 @@
 import { Link, LoaderFunction, useLoaderData } from "remix";
+import path from "path";
+import fs from "fs/promises";
 
 type Article = {
-  id: number;
+  id: number | string;
   title: string;
 };
 
-export const loader: LoaderFunction = (): Article[] => {
-  return [
-    { id: 1, title: "slovaci vyhrali" },
-    { id: 2, title: "slovaci prehrali" },
-  ];
+export const loader: LoaderFunction = (): Promise<Article[]> => {
+  const storagePath = path.resolve("storage", "articles");
+
+  return fs.readdir(storagePath).then((filenames) => {
+    return filenames.map((filename) => {
+      const [id, title] = filename.split("_");
+      return { id: id, title: title.replace(".mdx", "") };
+    });
+  });
 };
 
 export default function Index() {
