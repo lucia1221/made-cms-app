1f6f775726096233a0d8c602457b2c2feaa507f0
lucia1221 <58188855+lucia1221@users.noreply.github.com>
2022-01-14 11:35:51 +0100

    Delete article from admin panel (#9)
    Co-authored-by: lucia1221 <>

--- app/routes/admin/articles/index.tsx
+++ app/routes/admin/articles/index.tsx
@@ -1,4 +1,10 @@
-import { Link, LoaderFunction, useLoaderData } from "remix";
+import {
+  ActionFunction,
+  Form,
+  Link,
+  LoaderFunction,
+  useLoaderData,
+} from "remix";
 import { Article } from "~/models/article";
 import { databaseService } from "~/services/databaseService";
 
@@ -13,15 +19,37 @@ export const loader: LoaderFunction = async function (): Promise<Article[]> {
 
   return response.data;
 };
+export const action: ActionFunction = async function ({ request }) {
+  switch (request.method) {
+    case "DELETE":
+      const form = await request.formData();
+
+      await databaseService
+        .from("articles")
+        .delete()
+        .match({ id: form.get("id") });
+      break;
+  }
+
+  return null;
+};
 
 export default function AdminArticleListing() {
   const articles = useLoaderData<Article[]>();
+  function confirmDelete(event: any) {
+    if (window.confirm("Are you sure?") !== true) {
+      event.preventDefault();
+    }
+  }
 
   return articles.map((a) => (
-    <div key={a.id}>
+    <Form method="delete" key={a.id}>
+      <input defaultValue={a.id} name="id" type="hidden"></input>
       {a.title}
       <Link to={`/admin/articles/${a.id}`}>Edit</Link>
-      <button>Delete</button>
-    </div>
+      <button type="submit" onClick={confirmDelete}>
+        Delete
+      </button>
+    </Form>
   ));
 }
